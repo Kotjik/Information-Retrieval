0f58d1426c38f973229f89ce5c053cb2772149d9
Konstantin <kulik.konstantin@gmx.de>
2019-01-19 16:04:14 +0100

    Timestamp funktion angeschaut
    siehe logdata.js
    0index.html überarbeitet
    TODO:
    -html form to string?
    -wie logdata aufbauen?
    -in ein csv oder text datei alles reinschreiben

---
+++ res/js/logdata.js
@@ -0,0 +1,21 @@
+function getFormattedDate(){
+    var date = new Date();
+    var str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+              + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+              // + ":" + date.getMilliseconds();
+
+    return str;
+}
+
+
+function printInfo(elem, value){
+  console.log(
+    elem.parentNode.parentNode.parentNode.id + ", " + value + ", " + getFormattedDate()
+  );
+}
+
+function printLink(elem){
+  console.log(
+    elem.parentNode.parentNode.id + " " + getFormattedDate()
+  );
+}
